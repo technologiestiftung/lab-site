9d29affedee241f0c0233c07244a9c769bcf07a8
Sebastian Meier <contact@sebastianmeier.eu>
2017-10-26 17:47:08 +0200

    rearranging stuff

---
+++ js/script.js
@@ -0,0 +1,45 @@
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+}
+
+var embedResize = debounce(function() {
+	d3.selectAll('.embed').each(function(){
+		var w = d3.select(this).node().clientWidth;
+		d3.select(this).select('img,object,video,iframe').each(function(){
+			var cw = d3.select(this).attr('width'),
+				ch = d3.select(this).attr('height');
+			d3.select(this).style('height', Math.round(ch/cw*w)+'px');
+		});
+	});
+}, 250);
+
+if(d3.select('.embed').size()>=1){
+	window.addEventListener('resize', embedResize);
+	embedResize();
+}
+
+if(d3.select('#minilogo').size()>=1){
+	logo(d3.select('#minilogo'), 50, 5, 5, false, true, {min:0.2, max:1.5});
+	d3.selectAll('.gallery li')
+	    .classed('two-break', function(d,i){ return (i%2==0)?true:false; })
+	    .classed('three-break', function(d,i){ return (i%3==0)?true:false; });
+}else if(d3.select('#logo').size()>=1){
+	logo(d3.select('#logo'), 200, 20, 5, false, true, {min:0.2, max:2});
+	d3.selectAll('#project-list li')
+	    .classed('two-break', function(d,i){ return (i%2==0)?true:false; })
+	    .classed('three-break', function(d,i){ return (i%3==0)?true:false; });
+	d3.selectAll('#team-list li')
+	    .classed('two-break', function(d,i){ return (i%2==0)?true:false; })
+	    .classed('three-break', function(d,i){ return (i%3==0)?true:false; });
+}
