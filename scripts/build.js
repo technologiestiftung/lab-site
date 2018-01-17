27994a2ca6f4108e88886a4ba943549c525a53c8
Sebastian Meier <contact@sebastianmeier.eu>
2018-01-17 11:49:47 +0100

    SEO optimisation

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,13 @@
-let fs = require('fs')
+let fs = require('fs'),
+	moment = require('moment')
 
 let index_html = {
 	DE:fs.readFileSync('scripts/templates/index.html', 'utf8'),
 	EN:fs.readFileSync('scripts/templates/index_en.html', 'utf8')
+},
+feed_xml = {
+	DE:fs.readFileSync('scripts/templates/feed_de.xml', 'utf8'),
+	EN:fs.readFileSync('scripts/templates/feed_en.xml', 'utf8')
 }
 
 let project_path = 'docs/projects/',
@@ -18,8 +23,9 @@ fs.readdirSync(project_path).forEach(file => {
 			if(project.DATE.indexOf('/')==-1){
 				project.DATE = '12/2017'
 			}
-			let d = project.DATE.split('/')
-			project.sort = parseInt(d[1]) + parseInt(d[0])/24
+
+			let date = project.DATE.split('/');
+			project.sort = moment(date[2]+'-'+date[1]+'-'+date[0])
 			projects.push(project)
 
 			langs.forEach(lang=>{
@@ -38,7 +44,8 @@ projects.sort(function(a,b){
 })
 
 let filter_html = {EN:'',DE:''},
-	projects_html = {EN:'',DE:''}
+	projects_html = {EN:'',DE:''},
+	items_xml = {EN:'',DE:''}
 
 function strToValue(str){
 	if(str && str.length > 1){
@@ -78,9 +85,25 @@ langs.forEach(lang=>{
 		projects_html[lang] += '         	           </a>'+'\n'
 		projects_html[lang] += '                    </li>'+'\n'
 
+
+		items_xml[lang] += '\n'
+		items_xml[lang] += '<item>'+'\n'
+		items_xml[lang] += '<title><![CDATA['+p['PROJECT_TITLE_'+lang]+']]></title>'+'\n'
+		items_xml[lang] += '<link>http://lab.technologiestiftung-berlin.de/projects/'+p.PROJECT+'/index' + ((lang=='DE')?'':'_en') + '.html</link>'+'\n'
+		items_xml[lang] += '<pubDate>'+p.sort.format('ddd, D MMM YYYY HH:mm:ss ')+' +0100</pubDate>'+'\n'
+		items_xml[lang] += '<category><![CDATA['+p['TAG_'+lang]+']]></category>'+'\n'
+		items_xml[lang] += '<description><![CDATA['+p['PROJECT_DESCRIPTION_'+lang]+']]></description>'+'\n'
+		items_xml[lang] += '<content:encoded><![CDATA['+p['PROJECT_DESCRIPTION_'+lang]+']]></content:encoded>'+'\n'
+		items_xml[lang] += '<dc:creator><![CDATA['+p.AUTHOR_NAME+']]></dc:creator>'+'\n'
+		items_xml[lang] += '<guid isPermaLink="true">http://lab.technologiestiftung-berlin.de/projects/'+p.PROJECT+'/index' + ((lang=='DE')?'':'_en') + '.html</guid>'+'\n'
+		items_xml[lang] += '</item>'+'\n'
+
 	})
 	index_html[lang] = index_html[lang].replace('{{PROJECTLIST}}', projects_html[lang])
+	feed_xml[lang] = feed_xml[lang].replace('{{ITEMS}}', items_xml[lang])
+	feed_xml[lang] = feed_xml[lang].replace('{{TIMESTAMP}}', moment().format('ddd, D MMM YYYY HH:mm:ss ')+' +0100')
 
 	fs.writeFileSync('docs/index' + ((lang=='DE')?'':'_en') + '.html', index_html[lang], 'utf8')
+	fs.writeFileSync('docs/feed' + ((lang=='DE')?'':'_en') + '.xml', feed_xml[lang], 'utf8')
 })
 
