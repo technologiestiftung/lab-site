5a4ee17e1eb4d2c09d212724b64ffbce1ca26df6
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2019-02-05 22:41:15 +0100

    chore(bundled js): adds the bundled js

--- assets/js/index.bundle.js
+++ assets/js/index.bundle.js
@@ -326,7 +326,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63779" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60443" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
