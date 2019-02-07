3e5178a2250627bd8c67b41c85c2a77453bb8996
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2019-02-07 15:13:58 +0100

    chore(bundle): just bundle the js

--- source/assets/js/index.bundle.js
+++ source/assets/js/index.bundle.js
@@ -28877,7 +28877,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63359" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51370" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
@@ -29020,4 +29020,4 @@ function hmrAccept(bundle, id) {
   });
 }
 },{}]},{},["../../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/index.bundle.map
+//# sourceMappingURL=index.bundle.map
