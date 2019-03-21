ac476543ea16a90d341c5b73cec194432b0481ea
Fabian Morón Zirfas <icke@fabianmoronzirfas.me>
2019-03-21 16:08:25 +0100

    build: add grid autoprefix thing?

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+const postcssPresetEnv = require('postcss-preset-env');
+
+module.exports = {
+  plugins: [
+    postcssPresetEnv({autoprefixer: { grid: 'autoplace' }}),
+    require('autoprefixer'),
+    require('cssnano'),
+  ],
+  syntax: 'postcss-scss',
+};
