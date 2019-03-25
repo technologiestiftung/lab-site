a8fe322404c7bc4b2ab2fa55b9c5d2f0e863f583
fdnklg <fabian.dinklage@me.com>
2019-03-25 14:25:30 +0100

    fix: css grid ie11

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,12 @@ const postcssPresetEnv = require('postcss-preset-env');
 
 module.exports = {
   plugins: [
-    postcssPresetEnv({autoprefixer: { grid: 'autoplace' }}),
-    // require('autoprefixer'),
-    require('cssnano'),
+    postcssPresetEnv({
+      autoprefixer: { grid: 'autoplace' },
+    }),
+    // require('autoprefixer')({ grid: true }),
+    // require('postcss-unrgba'),
+    // require('cssnano')
   ],
   // syntax: 'postcss-scss',
 };
