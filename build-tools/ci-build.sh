7113b1fecb092c2365c9ae0d71d64d058beef4b3
Fabian Morón Zirfas <icke@fabianmoronzirfas.me>
2019-04-17 13:01:34 +0200

    ci: fix build script

--- build-tools/ci-build.sh
+++ build-tools/ci-build.sh
@@ -2,8 +2,6 @@
 # shellcheck source=/dev/null
 set -euo pipefail
 IFS=$'\n\t'
-# EXECPATH=$(pwd)
-source "${EXECPATH}/build-tools/env.sh"
 
 bundle exec jekyll build
 # bundle exec htmlproofer "./_site/" --disable-external
