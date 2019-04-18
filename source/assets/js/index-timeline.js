a599c5ea90499af5cf5f3f68e2f5d68d30af34d6
fdnklg <fabian.dinklage@me.com>
2019-04-18 08:25:04 +0200

    favicon updated for dark mode browsers

--- source/assets/js/index-timeline.js
+++ source/assets/js/index-timeline.js
@@ -29030,7 +29030,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57660" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63608" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
