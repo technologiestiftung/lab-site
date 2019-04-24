282b2fb6140ae654fb83aa1642d102d160fe7432
Fabian Dinklage <enjoi@enjoi.fritz.box>
2019-04-24 23:33:17 +0200

    more fixes

--- source/assets/js/index.js
+++ source/assets/js/index.js
@@ -9112,7 +9112,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55645" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54114" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
