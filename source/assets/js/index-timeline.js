6ce0f17930d0bd925eaef9e5bef48c98119467e3
fdnklg <fabian.dinklage@me.com>
2019-07-04 14:34:43 +0200

    core datasets site added at: /projects/odis-datasets/en

--- source/assets/js/index-timeline.js
+++ source/assets/js/index-timeline.js
@@ -29031,7 +29031,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60626" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58163" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
