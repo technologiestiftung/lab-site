8485652291d01d109825fe178da218afc57fbf27
fdnklg <fabian.dinklage@me.com>
2019-07-17 17:23:53 +0200

    odis datasets german version added

--- source/assets/js/index-timeline.js
+++ source/assets/js/index-timeline.js
@@ -29031,7 +29031,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51388" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57452" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
