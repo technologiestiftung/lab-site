c82d8cbe9a4f26d66a12d4f4caa2efb052d37155
ff6347 <fabianmoronzirfas@protonmail.ch>
2020-12-15 16:31:39 +0100

    style(eslint): Use eslint instead of tslint
    removed tslint since it is depreacted
    added @inpyjamas/scripts for eslint and prettier configs
    combined old eslint with new eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,29 @@
-module.exports = {
-  extends: [
-    'unobtrusive',
-    'eslint:recommended',
-    'plugin:jest/recommended'
-
-  ],
-  "plugins": ["jest"],
+const { merge } = require("@inpyjamas/scripts/utilities");
+const config = require("@inpyjamas/scripts/eslint");
+const oldConfig = {
+  extends: ["unobtrusive", "eslint:recommended", "plugin:jest/recommended"],
+  plugins: ["jest"],
   env: {
     browser: true,
     node: true,
-    "jest/globals": true
-
+    "jest/globals": true,
   },
   globals: {
-    "tns": true
+    tns: true,
   },
   rules: {
-    semi: [2, 'always'],
-    quotes: [2, 'single'],
+    semi: [2, "always"],
     indent: [2, 2],
-    'linebreak-style': [2, 'unix'],
-    'brace-style': [2, '1tbs'],
-    'array-bracket-spacing': [2, 'never'],
-    camelcase: [2, {properties: 'always'}],
-    'keyword-spacing': [2],
-    'eol-last': [2],
-    'no-trailing-spaces': [2],
-    'no-process-env': ['off', 'always'],
-    'no-console': ['off','always'],
-
-  }
+    "linebreak-style": [2, "unix"],
+    "brace-style": [2, "1tbs"],
+    "array-bracket-spacing": [2, "never"],
+    camelcase: [2, { properties: "always" }],
+    "keyword-spacing": [2],
+    "eol-last": [2],
+    "no-trailing-spaces": [2],
+    "no-process-env": ["off", "always"],
+    "no-console": ["off", "always"],
+  },
 };
+
+module.exports = merge(oldConfig, config);
