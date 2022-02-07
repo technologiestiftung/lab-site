b052e496f413c7706c72329244112182cdb13931
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2022-02-07 15:24:36 +0100

    refactor(IPLSITE-29): organize JS

--- source/assets/js/src/modules/archivalModal.js
+++ source/assets/js/src/modules/archivalModal.js
@@ -1,30 +1,21 @@
+const archivalModal = document.getElementById("archival-modal__wrapper");
+const archivalBanner = document.querySelector(".archival-banner");
+
+const removeModal = () => {
+  archivalModal.classList.add("archival-modal__wrapper--hidden");
+  archivalBanner.classList.remove("archival-banner--hidden");
+  document.querySelector("body").setAttribute("style", "overflow-y: auto;");
+};
+
+const showModal = () => {
+  archivalModal.classList.remove("archival-modal__wrapper--hidden");
+  archivalBanner.classList.add("archival-banner--hidden");
+  document.querySelector("body").setAttribute("style", "overflow-y: hidden;");
+};
+
 const initArchivalModal = () => {
-  const removeModal = () => {
-    const modal = document.getElementById("archival-modal__wrapper");
-    modal.classList.add("archival-modal__wrapper--hidden");
-
-    const archivalBanner = document.querySelector(".archival-banner");
-    archivalBanner.classList.remove("archival-banner--hidden");
-
-    document.querySelector("body").setAttribute("style", "overflow-y: auto;");
-  };
-
-  const showModal = () => {
-    const modal = document.getElementById("archival-modal__wrapper");
-    modal.classList.remove("archival-modal__wrapper--hidden");
-
-    const archivalBanner = document.querySelector(".archival-banner");
-    archivalBanner.classList.add("archival-banner--hidden");
-
-    document.querySelector("body").setAttribute("style", "overflow-y: hidden;");
-  };
-
   if (!window.sessionStorage.getItem("removed_modal")) {
     showModal();
-  }
-
-  if (window.sessionStorage.getItem("removed_modal") === true) {
-    removeModal();
   }
 
   const closeButton = document.getElementById("close-archival-modal");
